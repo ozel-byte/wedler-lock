b424109135660c42ab8ec8568ea034ddce4877ab
ozel-byte <ozelcode@gmail.com>
2024-06-15 13:45:38 -0600

    feat: add new section

--- lib/presentation/screens/my_passwords/section_my_password.dart
+++ lib/presentation/screens/my_passwords/section_my_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:passwordfamily/presentation/screens/home_page/widgets/input_search.dart';
+import 'package:passwordfamily/presentation/screens/my_passwords/widgets/box_my_passwords.dart';
+import 'package:passwordfamily/presentation/screens/my_passwords/widgets/button_new_password.dart';
 
 class SectionMyPassword extends StatelessWidget {
   const SectionMyPassword({super.key});
@@ -10,77 +12,28 @@ class SectionMyPassword extends StatelessWidget {
     return SizedBox(
         width: size.width,
         height: size.height,
-        child: Stack(
+        child: const Stack(
           children: [
             Card(
               margin: EdgeInsets.zero,
-              shape: const RoundedRectangleBorder(
+              shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                       topRight: Radius.circular(30))),
               child: Padding(
-                padding: const EdgeInsets.only(top: 10),
+                padding: EdgeInsets.only(top: 10),
                 child: Column(
                   children: [
-                    const InputSearch(),
-                    const SizedBox(
+                    InputSearch(),
+                    SizedBox(
                       height: 10,
                     ),
-                    Expanded(
-                      child: ListView.separated(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 70),
-                        separatorBuilder: (context, index) =>
-                            const Padding(padding: EdgeInsets.all(5)),
-                        itemCount: 10,
-                        itemBuilder: (BuildContext context, int index) {
-                          return const Card(
-                            elevation: 7,
-                            surfaceTintColor:
-                                Color.fromARGB(255, 215, 215, 215),
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                side: BorderSide(
-                                    strokeAlign: 1, color: Colors.grey)),
-                            child: ListTile(
-                              title: Text("Google account"),
-                              subtitle: Text("google@gmail.com"),
-                              trailing: Icon(Icons.arrow_forward_ios_outlined),
-                            ),
-                          );
-                        },
-                      ),
-                    )
+                    BoxMyPasswords()
                   ],
                 ),
               ),
             ),
-            Positioned(
-              bottom: 10,
-              left: 5,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      elevation: const MaterialStatePropertyAll(10),
-                      shadowColor: const MaterialStatePropertyAll(Colors.black),
-                      overlayColor:
-                          const MaterialStatePropertyAll(Colors.black),
-                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                      fixedSize:
-                          MaterialStatePropertyAll(Size(size.width - 10, 55)),
-                      backgroundColor: const MaterialStatePropertyAll(
-                          Color.fromARGB(255, 30, 30, 30)),
-                      foregroundColor:
-                          const MaterialStatePropertyAll(Colors.white)),
-                  onPressed: () {},
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                       Text("Add a new password"),
-                      Icon(Icons.add)
-                    ],
-                  )),
-            )
+            Positioned(bottom: 10, left: 5, child: ButtonNewPassword())
           ],
         ));
   }
